21385ef6b4d53258ac1085cdce5202893b049f8f
gustavoyamamoto-cloud <gustavoyamamoto@alunos.utfpr.edu.br>
2026-09-25 10:33:28 -0300

    criação do projeto

---
+++ gerenciamentoTarefas/src/test/java/gerenciamentoTarefas/GerenciamentoTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package gerenciamentoTarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

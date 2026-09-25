127fde5052369915f037b48b31df7efe0fa4f4a3
gustavoyamamoto-cloud <gustavoyamamoto@alunos.utfpr.edu.br>
2026-09-25 10:53:40 -0300

    adiciona entity tarefas

---
+++ gerenciamentoTarefas/src/main/java/gerenciamentoTarefas/entity/Tarefa.java
@@ -0,0 +1,58 @@
+package gerenciamentoTarefas.entity;
+
+import java.time.LocalDate;
+
+public class Tarefa {
+
+    private Long id;
+    private String titulo;
+    private String descricao;
+    private Status status;
+    private LocalDate dataCriacao;
+
+    public Tarefa() {}
+    
+    public Tarefa(Long id, String titulo, String descricao, Status status, LocalDate dataCriacao) {
+        this.id = id;
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.status = status;
+        this.dataCriacao = dataCriacao;
+    }
+
+    
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public Status getStatus() {
+		return status;
+	}
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+	public LocalDate getDataCriacao() {
+		return dataCriacao;
+	}
+	public void setDataCriacao(LocalDate dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
+
+    
+
+    
+}
